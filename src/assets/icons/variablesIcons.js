f4c8d7cb1a2ef8d8ae74d92e148500a6540aafc1
frojasCode <frojas.code@gmail.com>
2023-02-09 02:13:27 -0500

    add tabler-icons and setup variablesIcons

---
+++ src/assets/icons/variablesIcons.js
@@ -0,0 +1,11 @@
+/* COLORS */
+const colorPrimary = '#FF3E40';
+const colorDisabled = '#9F9F9F';
+
+/* SIZE */
+const size16 = 16;
+const size24 = 24;
+const size32 = 32;
+const size40 = 40;
+
+export { colorPrimary, colorDisabled, size16, size24, size32, size40 };
