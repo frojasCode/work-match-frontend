270973f6e90291ce03c90643a0aa4010b7649e57
Fernando Barrueto <63089348+Lirhblack@users.noreply.github.com>
2023-03-08 15:47:02 -0500

    Navbar link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,13 +2,16 @@ import { colorPrimary, size32 } from '../../assets/icons/variablesIcons';
 import { IconMenu2 } from '@tabler/icons-react';
 import { WorkMatchLogo } from '../../assets/icons/WorkMatchLogo';
 import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
 export function NavComponent() {
 	return (
 		<main>
 			<nav className='NavComponent'>
 				<div className='container'>
 					<div className='NavComponent-wrapper'>
-						<WorkMatchLogo size={32} />
+						<Link to="/">
+							<WorkMatchLogo size={32} />
+						</Link>
 						<IconMenu2 color={colorPrimary} size={size32} />
 					</div>
 				</div>
